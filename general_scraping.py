e7fb2fa65f54b0e7c534ab8ba864fe7c8b8643df
Raghav Kavimandan <raghavkavimandan23@gmail.com>
2022-04-25 16:15:28 +0530

    added functions

--- general_scraping.py
+++ general_scraping.py
@@ -21,82 +21,91 @@
 from distutils.log import error
 import requests
 from bs4 import BeautifulSoup
-url = "https://replit.com"
+
 headers = {'Accept': 'text/html'}
 
-r = requests.get(url, headers=headers)
-htmlContent = r.content
-soup = BeautifulSoup(htmlContent, 'html.parser')
-title = soup.title
-print('\033[1m' + f"Title = {title.text}" '\033[0m')
+def get_soup(url):
+    r = requests.get(url, headers=headers)
+    htmlContent = r.content
+    soup = BeautifulSoup(htmlContent, 'html.parser')
+    return soup
 
-try:
-    paras = soup.find_all('p')
-    if paras:
-        print(f"'\033[1m'+ Para = {paras.text} +'\033[0m'"  )
+def scrape(soup):
+    
+    title = soup.title
+    print('\033[1m' + f"Title = {title.text}" '\033[0m')
 
-except :
-    paras = soup.find('p')
-    print('\033[1m' + f"Para = {paras.text}" +'\033[0m')
+    try:
+        paras = soup.find_all('p')
+        if paras:
+            print(f"'\033[1m'+ Para = {paras.text} +'\033[0m'"  )
 
-try:
-    h1s = soup.find_all('h1')
-    if h1s:
+    except :
+        paras = soup.find('p')
+        print('\033[1m' + f"Para = {paras.text}" +'\033[0m')
+
+    try:
+        h1s = soup.find_all('h1')
+        if h1s:
+            print('\033[1m' + f"H1 = {h1s.text}" + '\033[0m', end='\n', sep='\n')
+
+    except :
+        h1s = soup.find('h1')
         print('\033[1m' + f"H1 = {h1s.text}" + '\033[0m', end='\n', sep='\n')
 
-except :
-    h1s = soup.find('h1')
-    print('\033[1m' + f"H1 = {h1s.text}" + '\033[0m', end='\n', sep='\n')
+
+    try:
+        
+        h2s = soup.find_all('h2')
+        if h2s:
+            print('\033[1m' + f"H2 = {h2s.text}" + '\033[0m', end='\n', sep='\n')
+
+    except :
+        h2s = soup.find('h2')
+        print('\033[1m' + f"H2 = {h2s.text}" +'\033[0m', end='\n', sep='\n')
 
 
-try:
-    
-    h2s = soup.find_all('h2')
-    if h2s:
-        print('\033[1m' + f"H2 = {h2s.text}" + '\033[0m', end='\n', sep='\n')
+def scrape_links(soup, url):
+    anchors = soup.find_all('a')
+    # all_links = set()
+    # for link in anchors:
+    #     if(link.get('href') != '#'): 
+    #         linkText = url +link.get('href')
+    #         all_links.add(link)
+    all_links = []
+    print("\n")
 
-except :
-    h2s = soup.find('h2')
-    print('\033[1m' + f"H2 = {h2s.text}" +'\033[0m', end='\n', sep='\n')
+    print('\033[1m' + "Links: " +'\033[0m')
+    for linkk in anchors:
+        if linkk['href'].startswith("https://"):
+            all_links.append(linkk['href'])
+        else:
+            all_links.append(url + linkk['href'])
 
+    for a_links in all_links:
+        print(a_links, end="\n")
 
+def scrape_images(soup, url):
+    image_tags = soup.find_all('img')
+    links = []
+    for image_tag in image_tags:
+        if image_tag['src'].startswith("https://"):
+            links.append(image_tag['src'])
+        else:
+            links.append(url + image_tag['src'])
 
-anchors = soup.find_all('a')
-# all_links = set()
-# for link in anchors:
-#     if(link.get('href') != '#'): 
-#         linkText = url +link.get('href')
-#         all_links.add(link)
-all_links = []
-print("\n")
+    print("\n")
 
-print('\033[1m' + "Links: " +'\033[0m')
-for linkk in anchors:
-    if linkk['href'].startswith("https://"):
-        all_links.append(linkk['href'])
-    else:
-        all_links.append(url + linkk['href'])
+    print('\033[1m' + "Images: " + '\033[0m', end='\n')
 
-for a_links in all_links:
-    print(a_links, end="\n")
+    for link in links:
+        print(link, end='\n')
 
-
-image_tags = soup.find_all('img')
-links = []
-for image_tag in image_tags:
-    if image_tag['src'].startswith("https://"):
-        links.append(image_tag['src'])
-    else:
-        links.append(url + image_tag['src'])
-
-print("\n")
-
-print('\033[1m' + "Images: " + '\033[0m', end='\n')
-
-for link in links:
-    print(link, end='\n')
-
-
+url = "https://reactjs.org"
+soup = get_soup(url)
+scrape(soup)
+scrape_links(soup, url)
+scrape_images(soup, url)
 # import urllib.request
 # urllib.request.urlretrieve(links[0], "Images/innovators.jpg")
 
